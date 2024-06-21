f75b6b41de07efaace8bebb08878679b683779e2
Sahand <mohsen.301933@gmail.com>
2024-06-21 21:18:42 +0330

    Enhanced login form with validation, added signup & password toggle
    - Refined Login Form: Improved the login form by incorporating constraint validation to enhance user input accuracy.
    
    - Sign-Up Form Initialization: Established the groundwork for a sign-up form to appear when users click on the "I don't have an account" text within the login section.
    
    - Container Positioning: Readjusted the positioning of side panels and the main form container, allowing each container to be moved individually for improved layout customization.
    
    - Password Visibility Toggle: Added an intuitive eye button feature for passwords, enabling users to toggle password visibility as needed. Additionally, adjusted input sizes for enhanced usability.
    
    - Spelling Error Correction: Addressed a spelling error within the login form to ensure a polished and error-free interface.

--- app.js
+++ app.js
@@ -1,24 +1,105 @@
-const formContainer = document.querySelector('.form-container')
-const formToggleText = document.querySelector('.toggle-form-anchor')
-formToggleText.addEventListener('click', () => {
-  formContainer.classList.toggle('right-side')
-})
+let isLoggingInMode = true
+function formToggle() {
+  const container = document.querySelector('.container')
+  const formContainer = document.querySelector('.form-container')
+  const loginForm = document.querySelector('.form-login')
+  const loginFormInfo = document.querySelector('.form-login .login-info')
+  const signUpForm = document.querySelector('.form-signup')
+  const signUpFormInfo = document.querySelector('.form-signup .signup-info')
+  
+  if (isLoggingInMode) {
+    // returns to default form container transition
+    formContainer.style.transform =
+      'translateX(calc(-100% + clamp(550px, 50vw, 650px)))'
+    formContainer.style.transition = 'transform 0.5s ease'
+    setTimeout(() => {
+      formContainer.style.transition = 'none'
+      formContainer.style.transform = 'none'
+      formContainer.style.left = 'auto'
+      formContainer.style.right = '0'
+    }, 500);
+    
+    // sign up form appears
+    container.style.height = '550px'
+    formContainer.classList.add('right-side')
+    loginForm.style.transition = 'opacity .35s ease, transform 0.35s ease'
+    signUpForm.style.transition = 'opacity .55s .1s ease, transform 0.45s .08s ease'
+    loginFormInfo.style.transform = 'translateX(-35%)'
+    signUpFormInfo.style.transform = 'translateX(0%)'
+    
+    isLoggingInMode = false
+  } else {
+    // removes form container transition
+    formContainer.style.transform =
+      'translateX(calc(100% - clamp(550px, 50vw, 650px)))'
+    formContainer.style.transition = 'transform 0.5s ease'
+    setTimeout(() => {
+      formContainer.style.transition = 'none'
+      formContainer.style.transform = 'none'
+      formContainer.style.left = '0'
+      formContainer.style.right = 'auto'
+    }, 500);
+    
+    // login form appears
+    container.style.height = '500px'
+    formContainer.classList.remove('right-side')
+    loginForm.style.transition = 'opacity .55s .1s ease, transform 0.45s .08s ease'
+    signUpForm.style.transition = 'opacity .35s ease, transform 0.2s ease'
+    loginFormInfo.style.transform = 'translateX(0%)'
+    signUpFormInfo.style.transform = 'translateX(35%)'
+    
+    isLoggingInMode = true
+  }
+}
 
-const loginBtn = document.querySelector('.submit-btn')
-const loginBtnText = document.querySelector('.submit-btn .login-submit-text')
-const loadingContainer = document.querySelector('.submit-btn .submit-loading-container')
-let loginAttemptSucceeded = false
+const passToggleBtns = document.querySelectorAll('.passToggleBtn');
+
+passToggleBtns.forEach((btn) => {
+  const passwordInput =
+    btn.parentElement.querySelectorAll('input[type="password"]');
+  const visibleIcon = btn.querySelector('.visibleIcon');
+  const invisibleIcon = btn.querySelector('.invisibleIcon');
+  let isPasswordFieldVisible = false;
+  
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    passwordInput.forEach((input) => {
+      if (isPasswordFieldVisible) {
+        input.type = 'password';
+        visibleIcon.style.opacity = '1';
+        invisibleIcon.style.opacity = '0';
+        isPasswordFieldVisible = false;
+      } else {
+        input.type = 'text';
+        visibleIcon.style.opacity = '0';
+        invisibleIcon.style.opacity = '1';
+        isPasswordFieldVisible = true;
+      }
+      
+      passwordInput.focus();
+    });
+  });
+});
+
+
+const loginBtn = document.querySelector('.login-submit-btn')
+const loginBtnText = document.querySelector('.login-submit-btn .login-submit-text')
+const loadingContainer = document.querySelector('.login-submit-btn .submit-loading-container')
+let loginAttemptMade = false
 
 loginBtn.addEventListener('click', e => {
   e.preventDefault()
-  APP.validate()
   
-  if (!loginAttemptSucceeded) {
-    const inputFields = document.querySelectorAll('.input-field');
+  
+  if (!loginAttemptMade) {
+    const inputFields = document.querySelectorAll('.login-info .input-field');
     
+    // handling errors
     let foundMismatch = false
     inputFields.forEach((inputField) => {
       if (inputField.classList.contains('error-visible')) {
+        // Error animation on repeated false input
         let errorIcon = inputField.querySelector('.errorIcon')
         errorIcon.classList.add('wobble-animation')
         setTimeout(() => {
@@ -29,12 +110,15 @@ loginBtn.addEventListener('click', e => {
         inputField.classList.contains('invalidInput') ||
         inputField.firstElementChild.value === ''
       ) {
+        // Error animation on first false input
         inputField.classList.add('error-visible')
         foundMismatch = true
       }
     });
     
-    if (!foundMismatch) {
+    // login validation
+    if (!foundMismatch && validateLogin()) {
+      // Login success button animation
       loginBtnText.style.opacity = 0
       loadingContainer.style.opacity = 1
       loginBtn.classList.add('loading')
@@ -49,7 +133,9 @@ loginBtn.addEventListener('click', e => {
         loginBtn.classList.add('success')
       }, 3000)
       
-      loginAttemptSucceeded = true
+      loginAttemptMade = true
+    } else {
+      // Login fail button animation
     }
   }
 })
@@ -74,25 +160,98 @@ const APP = {
     APP.addListener()
   },
   addListener() {
-    // let form = document.querySelector('.form-login form')
-    const emailField = document.querySelector('.form-login #email')
-    const passwordField = document.querySelector('.form-login #pass')
-    emailField.addEventListener('input', () => {
-      APP.testEmail(emailField)
+    const loginEmailField = document.querySelector('.form-login #loginEmail')
+    const loginPasswordField = document.querySelector('.form-login #loginPass')
+    const signUpEmailField = document.querySelector('.form-signup #signUpEmail')
+    const signUpPassField = document.querySelector('.form-signup #signUpPass')
+    const signUpPassConfirmField = document.querySelector('.form-signup #signUpPassConfirm')
+    
+    loginEmailField.addEventListener('input', () => {
+      APP.testEmail(loginEmailField)
     })
-    passwordField.addEventListener('change', () => {
-      updateInputField(emailField, true)
+    loginPasswordField.addEventListener('input', () => {
+      APP.testPassword(loginPasswordField)
     })
   },
-  validate() {
-    const emailField = document.getElementById('email')
-    const passwordField = document.getElementById('email')
-    
+  validateLogin() {
+    const email = document.getElementById('loginEmail').value
+    const password = document.getElementById('loginPass').value
+    // check with database
+    return true
+  },
+  validateSignUp() {
+    // add to database
   },
   testEmail(emailField) {
+    if (emailField.value.length > 22) {
+      emailField.style.fontSize = '0.75rem'
+    } else if (emailField.value.length > 17) {
+      emailField.style.fontSize = '0.88rem'
+    } else {
+      emailField.style.fontSize = '1.1rem'
+    }
     let re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     let isMatch = re.test(emailField.value)
     updateInputField(emailField, isMatch)
+  },
+  testPassword(passField) {
+    if (passField.value.length > 3 && passField.value.length < 10) {
+      updateInputField(passField, true)
+    } else {
+      updateInputField(passField, false)
+    }
+    
+    if (passField.value.length > 22) {
+      passField.style.fontSize = '0.75rem'
+    } else if (passField.value.length > 15) {
+      passField.style.fontSize = '0.88rem'
+    } else {
+      passField.style.fontSize = '1.1rem'
+    }
   }
 }
 document.addEventListener('DOMContentLoaded', APP.init)
+
+// mapping international dialing codes to country names
+const sortedCountryCodes = {
+  "1": "United States",
+  "7": "Ukraine",
+  "20": "Egypt",
+  "27": "South Africa",
+  "30": "Cyprus",
+  "33": "Spain",
+  "36": "Hungary",
+  "39": "Italy",
+  "41": "Switzerland",
+  "44": "Ireland",
+  "44": "United Kingdom",
+  "46": "Norway",
+  "48": "Poland",
+  "49": "Austria", 
+  "52": "Colombia",
+  "55": "Argentina",
+  "58": "Venezuela",
+  "60": "Vietnam",
+  "61": "New Zealand",
+  "62": "Malaysia",
+  "63": "Philippines",
+  "65": "Singapore",
+  "82": "Vietnam",
+  "86": "Canada",
+  "91": "India",
+  "92": "Pakistan",
+  "234": "Kenya",
+  "255": "Tanzania",
+  "351": "Portugal",
+  "420": "Czech Republic",
+  "501": "Belize",
+  "507": "Panama",
+  "507": "Panama",
+  "213": "Algeria",
+  "230": "Mauritius",
+  "234": "Ghana",
+  "507": "Panama",
+  "503": "Portugal",
+  "507": "Panama",
+  "972": "Israel",
+};
